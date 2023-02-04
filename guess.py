19e2ba80597d4e52ae5baef8a242e90892d94130
cascade7 <yiminlee777@gmail.com>
2023-02-04 10:47:15 +0800

    version2

--- guess.py
+++ guess.py
@@ -1,12 +1,16 @@
 import random
 r = random.randint(1,100)
+count=0
 while True :
+	count += 1
 	n = input('請輸入一個數字: ')
 	n = int(n)
 	if n == r:
 		print('答對了')
+		print('這是你猜的', count, '次')
 		break
 	elif n > r:
 		print('比答案大')
 	elif n < r:
 		print('比答案小')
+	print('這是你猜的', count, '次')
